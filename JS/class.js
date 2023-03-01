46da7572a3e84d50b5affd5f14a3361cb407125f
Alejandro <ralejoospi@gmail.com>
2023-03-01 11:36:04 -0500

    final clase

--- JS/class.js
+++ JS/class.js
@@ -1,3 +1,26 @@
-console.log("hola mundo");
 
-console.log(Math.random());
+
+//flecha sin parametros
+const saludar = () => {
+    return console.log("Hola, ¿cómo estás?");
+};
+saludar();
+
+//flecha 1 parametro
+const doble = numero => {
+    return numero * 2;
+};
+console.log(doble(5));
+
+//flecha 2 parametros
+const double = (a, b) => {
+    return a + b;
+};
+console.log(double(1,2));
+
+//arreglo
+let numeros = [1,2,3,4,5,6];
+//ingresa un numero adicional
+numeros.push(10);
+//saca el ultimo elemento
+numeros.pop();
---
+++ jaja.py
@@ -0,0 +1,8 @@
+num = 5
+
+if(num > 0):
+    print("ja")
+elif(num < 10):
+    print("ja")
+else:
+    print("no")
